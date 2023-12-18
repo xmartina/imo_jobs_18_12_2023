67bcceec764347ede7c9292e6413fd062c4853ca
xmartina <martinsprince126@gmail.com>
2023-12-18 11:41:40 +0100

    d

---
+++ resources/views/front_web/layouts/pre_loader.blade.php
@@ -0,0 +1,9 @@
+<div id="preloader-active">
+    <div class="preloader d-flex align-items-center justify-content-center">
+        <div class="spinner">
+            <div class="bounce1"></div>
+            <div class="bounce2"></div>
+            <div class="bounce3"></div>
+        </div>
+    </div>
+</div>
