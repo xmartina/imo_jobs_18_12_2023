df02a12afb39a97a16328dc2cf7e4259a5f34a29
xmartina <martinsprince126@gmail.com>
2023-12-19 11:08:25 +0100

    d

--- resources/views/front_web/layouts/new_template_footer_script.blade.php
+++ resources/views/front_web/layouts/new_template_footer_script.blade.php
@@ -1,58 +1,58 @@
 <link rel='stylesheet' id='cup-css' href='https://jthemes.com/themes/wp/jobbox/home/customer/www/jthemes.com/public_html/themes/wp/jobbox/wp-content/plugins/control-user-profileassets/user-profile.css?ver=1.0.0' media='all'>
-<link rel='stylesheet' id='wp-color-picker-css' href="{{ asset('wp-admin/css/color-picker.min.css?ver=6.4.2') }}" media='all'>
-<link rel='stylesheet' id='ctrlbp-css' href="{{ asset('wp-content/plugins/control-block-patterns/assets/css/ctrlbp-admin.min.css?ver=1.3.5.4') }}" media='all'>
-<script src="{{ asset('wp-content/plugins/contact-form-7/includes/swv/js/index.js?ver=5.8.4')}}" id="swv-js"></script>
-<script src="{{ asset('wp-content/plugins/contact-form-7/includes/js/index.js?ver=5.8.4')}}" id="contact-form-7-js"></script>
-<script src="{{ asset('wp-content/plugins/control-job-manager/assets/js/bootstrap.bundle.min.js?ver=5.2.0')}}" id="bootstrap-bundle-js"></script>
-<script src="{{ asset('wp-includes/js/jquery/ui/core.min.js?ver=1.13.2')}}" id="jquery-ui-core-js"></script>
-<script src="{{ asset('wp-includes/js/jquery/ui/menu.min.js?ver=1.13.2')}}" id="jquery-ui-menu-js"></script>
-<script src="{{ asset('wp-includes/js/dist/vendor/wp-polyfill-inert.min.js?ver=3.1.2')}}" id="wp-polyfill-inert-js"></script>
-<script src="{{ asset('wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.14.0')}}" id="regenerator-runtime-js"></script>
-<script src="{{ asset('wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0')}}" id="wp-polyfill-js"></script>
-<script src="{{ asset('wp-includes/js/dist/dom-ready.min.js?ver=392bdd43726760d1f3ca')}}" id="wp-dom-ready-js"></script>
-<script src="{{ asset('wp-includes/js/dist/hooks.min.js?ver=c6aec9a8d4e5a5d543a1')}}" id="wp-hooks-js"></script>
-<script src="{{ asset('wp-includes/js/dist/i18n.min.js?ver=7701b0c3857f914212ef')}}" id="wp-i18n-js"></script>
-<script src="{{ asset('wp-includes/js/dist/a11y.min.js?ver=7032343a947cfccf5608" id="wp-a11y-js')}}"></script>
+<link rel='stylesheet' id='wp-color-picker-css' href="{{ asset('new_template/wp-admin/css/color-picker.min.css?ver=6.4.2') }}" media='all'>
+<link rel='stylesheet' id='ctrlbp-css' href="{{ asset('new_template/wp-content/plugins/control-block-patterns/assets/css/ctrlbp-admin.min.css?ver=1.3.5.4') }}" media='all'>
+<script src="{{ asset('new_template/wp-content/plugins/contact-form-7/includes/swv/js/index.js?ver=5.8.4')}}" id="swv-js"></script>
+<script src="{{ asset('new_template/wp-content/plugins/contact-form-7/includes/js/index.js?ver=5.8.4')}}" id="contact-form-7-js"></script>
+<script src="{{ asset('new_template/wp-content/plugins/control-job-manager/assets/js/bootstrap.bundle.min.js?ver=5.2.0')}}" id="bootstrap-bundle-js"></script>
+<script src="{{ asset('new_template/wp-includes/js/jquery/ui/core.min.js?ver=1.13.2')}}" id="jquery-ui-core-js"></script>
+<script src="{{ asset('new_template/wp-includes/js/jquery/ui/menu.min.js?ver=1.13.2')}}" id="jquery-ui-menu-js"></script>
+<script src="{{ asset('new_template/wp-includes/js/dist/vendor/wp-polyfill-inert.min.js?ver=3.1.2')}}" id="wp-polyfill-inert-js"></script>
+<script src="{{ asset('new_template/wp-includes/js/dist/vendor/regenerator-runtime.min.js?ver=0.14.0')}}" id="regenerator-runtime-js"></script>
+<script src="{{ asset('new_template/wp-includes/js/dist/vendor/wp-polyfill.min.js?ver=3.15.0')}}" id="wp-polyfill-js"></script>
+<script src="{{ asset('new_template/wp-includes/js/dist/dom-ready.min.js?ver=392bdd43726760d1f3ca')}}" id="wp-dom-ready-js"></script>
+<script src="{{ asset('new_template/wp-includes/js/dist/hooks.min.js?ver=c6aec9a8d4e5a5d543a1')}}" id="wp-hooks-js"></script>
+<script src="{{ asset('new_template/wp-includes/js/dist/i18n.min.js?ver=7701b0c3857f914212ef')}}" id="wp-i18n-js"></script>
+<script src="{{ asset('new_template/wp-includes/js/dist/a11y.min.js?ver=7032343a947cfccf5608" id="wp-a11y-js')}}"></script>
 
-<script src="{{ asset('wp-includes/js/jquery/ui/autocomplete.min.js?ver=1.13.2" id="jquery-ui-autocomplete-js')}}"></script>
+<script src="{{ asset('new_template/wp-includes/js/jquery/ui/autocomplete.min.js?ver=1.13.2" id="jquery-ui-autocomplete-js')}}"></script>
 
-<script src="{{ asset('wp-content/plugins/control-job-manager/assets/js/control-job-manager.js?ver=1.1.7" id="control-job-manager-js')}}"></script>
-<script src="{{ asset('wp-content/plugins/control-job-manager/vendor/bookmarks/assets/dist/js/bookmark.js?ver=1.1.7')}}" id="wp-job-manager-bookmarks-bookmark-js-js"></script>
-<script src="{{ asset('wp-content/plugins/control-job-manager/vendor/tags/assets/dist/js/tag-filter.js?ver=1.1.7')}}" id="wp-job-manager-ajax-tag-filters-js"></script>
-<script src="{{ asset('wp-content/themes/jobbox/assets/js/vendor/modernizr-3.6.0.min.js?ver=3.6.0')}}" id="modernizr-js"></script>
-<script src="{{ asset('wp-content/themes/jobbox/assets/js/plugins/waypoints.js?ver=5.0.0')}}" id="waypoints-js"></script>
-<script src="{{ asset('wp-content/themes/jobbox/assets/js/plugins/counterup.js?ver=1.0') }}" id="counterup-js"></script>
-<script src="{{ asset('wp-content/themes/jobbox/assets/js/plugins/wow.js?ver=5.0.0') }}" id="wow-js"></script>
-<script src="{{ asset('wp-content/themes/jobbox/assets/js/plugins/magnific-popup.js?ver=5.0.0') }}" id="magnific-popup-js"></script>
-<script src="{{ asset('wp-content/themes/jobbox/assets/js/plugins/perfect-scrollbar.min.js?ver=5.0.0') }}" id="perfect-scrollbar-js"></script>
-<script src="{{ asset('wp-content/plugins/woocommerce/assets/js/select2/select2.full.min.js?ver=4.0.3-wc.8.3.1') }}" id="select2-js" defer="" data-wp-strategy="defer"></script>
-<script src="{{ asset('wp-content/themes/jobbox/assets/js/plugins/isotope.js?ver=5.0.0') }}" id="isotope-js"></script>
-<script src="{{ asset('wp-content/themes/jobbox/assets/js/plugins/scrollup.js?ver=5.0.0') }}" id="scrollup-js"></script>
-<script src="{{ asset('wp-content/themes/jobbox/assets/js/plugins/swiper-bundle.min.js?ver=5.0.0') }}" id="swiper-bundle-js"></script>
-<script src="{{ asset('wp-includes/js/imagesloaded.min.js?ver=5.0.0') }}" id="imagesloaded-js"></script>
-<script src="{{ asset('wp-includes/js/masonry.min.js?ver=4.2.2') }}" id="masonry-js"></script>
-<script src="{{ asset('wp-includes/js/jquery/jquery.masonry.min.js?ver=3.1.2b') }}" id="jquery-masonry-js"></script>
-<script src="{{ asset('wp-content/themes/jobbox/assets/js/main.js?ver=5.0.0') }}" id="jobbox-main-js"></script>
-<script src="{{ asset('wp-content/plugins/elementor/assets/lib/jquery-numerator/jquery-numerator.min.js?ver=0.2.1') }}" id="jquery-numerator-js"></script>
-<script src="{{ asset('wp-content/plugins/control-email-subscriber/jquery.ajaxchimp.min.js?ver=1.3.0') }}" id="jquery-ajaxchimp-js"></script>
-<script src="{{ asset('wp-content/plugins/control-email-subscriber/jquery.ajaxchimp.langs.min.js?ver=1.3.0') }}" id="jquery-ajaxchimp-langs-js"></script>
+<script src="{{ asset('new_template/wp-content/plugins/control-job-manager/assets/js/control-job-manager.js?ver=1.1.7" id="control-job-manager-js')}}"></script>
+<script src="{{ asset('new_template/wp-content/plugins/control-job-manager/vendor/bookmarks/assets/dist/js/bookmark.js?ver=1.1.7')}}" id="wp-job-manager-bookmarks-bookmark-js-js"></script>
+<script src="{{ asset('new_template/wp-content/plugins/control-job-manager/vendor/tags/assets/dist/js/tag-filter.js?ver=1.1.7')}}" id="wp-job-manager-ajax-tag-filters-js"></script>
+<script src="{{ asset('new_template/wp-content/themes/jobbox/assets/js/vendor/modernizr-3.6.0.min.js?ver=3.6.0')}}" id="modernizr-js"></script>
+<script src="{{ asset('new_template/wp-content/themes/jobbox/assets/js/plugins/waypoints.js?ver=5.0.0')}}" id="waypoints-js"></script>
+<script src="{{ asset('new_template/wp-content/themes/jobbox/assets/js/plugins/counterup.js?ver=1.0') }}" id="counterup-js"></script>
+<script src="{{ asset('new_template/wp-content/themes/jobbox/assets/js/plugins/wow.js?ver=5.0.0') }}" id="wow-js"></script>
+<script src="{{ asset('new_template/wp-content/themes/jobbox/assets/js/plugins/magnific-popup.js?ver=5.0.0') }}" id="magnific-popup-js"></script>
+<script src="{{ asset('new_template/wp-content/themes/jobbox/assets/js/plugins/perfect-scrollbar.min.js?ver=5.0.0') }}" id="perfect-scrollbar-js"></script>
+<script src="{{ asset('new_template/wp-content/plugins/woocommerce/assets/js/select2/select2.full.min.js?ver=4.0.3-wc.8.3.1') }}" id="select2-js" defer="" data-wp-strategy="defer"></script>
+<script src="{{ asset('new_template/wp-content/themes/jobbox/assets/js/plugins/isotope.js?ver=5.0.0') }}" id="isotope-js"></script>
+<script src="{{ asset('new_template/wp-content/themes/jobbox/assets/js/plugins/scrollup.js?ver=5.0.0') }}" id="scrollup-js"></script>
+<script src="{{ asset('new_template/wp-content/themes/jobbox/assets/js/plugins/swiper-bundle.min.js?ver=5.0.0') }}" id="swiper-bundle-js"></script>
+<script src="{{ asset('new_template/wp-includes/js/imagesloaded.min.js?ver=5.0.0') }}" id="imagesloaded-js"></script>
+<script src="{{ asset('new_template/wp-includes/js/masonry.min.js?ver=4.2.2') }}" id="masonry-js"></script>
+<script src="{{ asset('new_template/wp-includes/js/jquery/jquery.masonry.min.js?ver=3.1.2b') }}" id="jquery-masonry-js"></script>
+<script src="{{ asset('new_template/wp-content/themes/jobbox/assets/js/main.js?ver=5.0.0') }}" id="jobbox-main-js"></script>
+<script src="{{ asset('new_template/wp-content/plugins/elementor/assets/lib/jquery-numerator/jquery-numerator.min.js?ver=0.2.1') }}" id="jquery-numerator-js"></script>
+<script src="{{ asset('new_template/wp-content/plugins/control-email-subscriber/jquery.ajaxchimp.min.js?ver=1.3.0') }}" id="jquery-ajaxchimp-js"></script>
+<script src="{{ asset('new_template/wp-content/plugins/control-email-subscriber/jquery.ajaxchimp.langs.min.js?ver=1.3.0') }}" id="jquery-ajaxchimp-langs-js"></script>
 <script id="control-email-subscriber-js-extra">
 
 </script>
-<script src="{{ asset('wp-content/plugins/control-email-subscriber/scripts.js?ver=1.0.0') }}" id="control-email-subscriber-js"></script>
+<script src="{{ asset('new_template/wp-content/plugins/control-email-subscriber/scripts.js?ver=1.0.0') }}" id="control-email-subscriber-js"></script>
 
-<script src="{{ asset('wp-includes/js/zxcvbn-async.min.js?ver=1.0') }}" id="zxcvbn-async-js"></script>
-<script src="{{ ('wp-admin/js/password-strength-meter.min.js?ver=6.4.2') }}" id="password-strength-meter-js"></script>
+<script src="{{ asset('new_template/wp-includes/js/zxcvbn-async.min.js?ver=1.0') }}" id="zxcvbn-async-js"></script>
+<script src="{{ ('new_template/wp-admin/js/password-strength-meter.min.js?ver=6.4.2') }}" id="password-strength-meter-js"></script>
 <script src="https://jthemes.com/themes/wp/jobbox/home/customer/www/jthemes.com/public_html/themes/wp/jobbox/wp-content/plugins/control-user-profileassets/user-profile.js?ver=1.0.0" id="mbup-js"></script>
-<script src="{{ asset('wp-content/plugins/control-block-patterns/assets/js/script.js?ver=1.3.5.4') }}" id="ctrlbp-js"></script>
+<script src="{{ asset('new_template/wp-content/plugins/control-block-patterns/assets/js/script.js?ver=1.3.5.4') }}" id="ctrlbp-js"></script>
 
-<script src="{{ asset('wp-content/plugins/control-block-patterns/assets/js/validation.min.js?ver=1.3.5.4') }}" id="ctrlbp-validation-js"></script>
-<script src="{{ asset('wp-content/plugins/control-block-patterns/assets/js/tabs.js?ver=1.1.7') }}" id="ctrlbp-tabs-js"></script>
-<script src="{{ asset('wp-content/plugins/control-block-patterns/assets/js/input-list.js?ver=1.3.5.4') }}" id="ctrlbp-input-list-js"></script>
-<script src="{{ asset('wp-content/plugins/elementor/assets/js/webpack.runtime.min.js?ver=3.18.1') }}" id="elementor-webpack-runtime-js"></script>
-<script src="{{ asset('wp-content/plugins/elementor/assets/js/frontend-modules.min.js?ver=3.18.1') }}" id="elementor-frontend-modules-js"></script>
-<script src="{{ asset('wp-content/plugins/elementor/assets/lib/waypoints/waypoints.min.js?ver=4.0.2') }}" id="elementor-waypoints-js"></script>
+<script src="{{ asset('new_template/wp-content/plugins/control-block-patterns/assets/js/validation.min.js?ver=1.3.5.4') }}" id="ctrlbp-validation-js"></script>
+<script src="{{ asset('new_template/wp-content/plugins/control-block-patterns/assets/js/tabs.js?ver=1.1.7') }}" id="ctrlbp-tabs-js"></script>
+<script src="{{ asset('new_template/wp-content/plugins/control-block-patterns/assets/js/input-list.js?ver=1.3.5.4') }}" id="ctrlbp-input-list-js"></script>
+<script src="{{ asset('new_template/wp-content/plugins/elementor/assets/js/webpack.runtime.min.js?ver=3.18.1') }}" id="elementor-webpack-runtime-js"></script>
+<script src="{{ asset('new_template/wp-content/plugins/elementor/assets/js/frontend-modules.min.js?ver=3.18.1') }}" id="elementor-frontend-modules-js"></script>
+<script src="{{ asset('new_template/wp-content/plugins/elementor/assets/lib/waypoints/waypoints.min.js?ver=4.0.2') }}" id="elementor-waypoints-js"></script>
 
-<script src="{{ asset('wp-content/plugins/elementor/assets/js/frontend.min.js?ver=3.18.1') }}" id="elementor-frontend-js"></script>
+<script src="{{ asset('new_template/wp-content/plugins/elementor/assets/js/frontend.min.js?ver=3.18.1') }}" id="elementor-frontend-js"></script>
 
 <script src="{{ asset('new_template/adjustments.footer.js') }}"></script>
