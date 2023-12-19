e60c06825efbdc5dc3f4d3a39d9f5a4e492f367c
xmartina <martinsprince126@gmail.com>
2023-12-19 08:44:34 +0100

    d

---
+++ resources/views/front_web/layouts/new_template.blade.php
@@ -0,0 +1,50 @@
+
+    <meta name='robots' content='max-image-preview:large'>
+    <link rel='dns-prefetch' href='https://code.jquery.com'>
+    <link rel='dns-prefetch' href='https://fonts.googleapis.com'>
+    <link rel="stylesheet" href="{{asset('new_template/adjustments.css')}}" id="wp-emoji-styles-inline-css">
+{{--    <link rel="stylesheet" href="{{asset('new_template/adjustments.css')}}" id="global-styles-inline-css">--}}
+    <script src="{{asset('new_template/adjustments.js')}}"></script>
+
+
+    <link rel='stylesheet' id='contact-form-7-css' href="{{ asset ('new_template/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=5.8.4') }}" media='all'>
+    <link rel='stylesheet' id='wp-job-manager-bookmarks-frontend-css' href="{{ asset ('new_template/wp-content/plugins/control-job-manager/vendor/bookmarks/assets/dist/css/frontend.css?ver=1.1.7') }}" media='all'>
+    <link rel='stylesheet' id='wp-job-manager-tags-frontend-css' href="{{ asset ('new_template/wp-content/plugins/control-job-manager/vendor/tags/assets/dist/css/frontend.css?ver=1.1.7') }}" media='all'>
+    <link rel='stylesheet' id='jm-application-deadline-css' href="{{ asset ('new_template/wp-content/plugins/control-job-manager/vendor/application-deadline/assets/dist/css/frontend.css?ver=1.1.7') }}" media=''>
+    <style id='woocommerce-inline-inline-css'>
+        .woocommerce form .form-row .required { visibility: visible; }
+    </style>
+    <link rel='stylesheet' id='wp-job-manager-job-listings-css' href="{{ asset ('new_template/wp-content/plugins/wp-job-manager/assets/dist/css/job-listings.css?ver=598383a28ac5f9f156e4') }}" media='all'>
+    <link rel='stylesheet' id='jobbox-normalize-css' href="{{ asset ('new_template/wp-content/themes/jobbox/assets/css/vendors/normalize.css?ver=8.0.1') }}" media='all'>
+    <link rel='stylesheet' id='bootstrap-css' href="{{ asset ('new_template/wp-content/themes/jobbox/assets/css/bootstrap/bootstrap.css?ver=5.2.2') }}" media='all'>
+    <link rel='stylesheet' id='jobbox-uicons-css' href="{{ asset ('new_template/wp-content/themes/jobbox/assets/css/vendors/uicons-regular-rounded.css?ver=1.0.0') }}" media='all'>
+    <link rel='stylesheet' id='fontawesome-css' href="{{ asset ('new_template/wp-content/themes/jobbox/assets/css/all.min.css?ver=6.2.0') }}" media='all'>
+    <link rel='stylesheet' id='jobbox-swiper-css' href="{{ asset ('new_template/wp-content/themes/jobbox/assets/css/plugins/swiper-bundle.min.css?ver=8.3.2') }}" media='all'>
+    <link rel='stylesheet' id='jobbox-magnific-css' href="{{ asset ('new_template/wp-content/themes/jobbox/assets/css/plugins/magnific-popup.css?ver=1.1.0') }}" media='all'>
+    <link rel='stylesheet' id='jobbox-select2-css' href="{{ asset ('new_template/wp-content/themes/jobbox/assets/css/plugins/select2.min.css?ver=1.0.0') }}" media='all'>
+    <link rel='stylesheet' id='jobbox-perfect-scrollbar-css' href="{{ asset ('new_template/wp-content/themes/jobbox/assets/css/plugins/perfect-scrollbar.css?ver=1.0.0') }}" media='all'>
+    <link rel='stylesheet' id='jobbox-animate-css' href="{{ asset ('new_template/wp-content/themes/jobbox/assets/css/plugins/animate.min.css?ver=3.5.2') }}" media='all'>
+    <link rel='stylesheet' id='jobbox-theme-css' href="{{ asset ('new_template/wp-content/themes/jobbox/assets/css/theme.css?ver=1.0.0') }}" media='all'>
+    <link rel='stylesheet' id='jobbox-style-css' href="{{ asset ('new_template/wp-content/themes/jobbox/assets/css/style.css?ver=1.0.0') }}" media='all'>
+    <link rel='stylesheet' id='woocommerce-css' href="{{ asset ('new_template/wp-content/themes/jobbox/assets/css/woocommerce.css?ver=1.2.0') }}" media='all'>
+    <link rel='stylesheet' id='wp-job-manager-css' href="{{ asset ('new_template/wp-content/themes/jobbox/assets/css/wp-job-manager.css?ver=1.2.0') }}" media='all'>
+    <link rel='stylesheet' id='jobbox-css' href="{{ asset ('new_template/wp-content/themes/jobbox/style.css?ver=6.4.2') }}" media='all'>
+    <link rel='stylesheet' id='control-elementor-css' href="{{ asset ('new_template/wp-content/plugins/control-elementor/assets/css/control-elementor.css?ver=1.0.0') }}" media='all'>
+    <link rel='stylesheet' id='elementor-icons-css' href="{{ asset ('new_template/wp-content/plugins/elementor/assets/lib/eicons/css/elementor-icons.min.css?ver=5.25.0') }}" media='all'>
+    <link rel='stylesheet' id='elementor-frontend-css' href="{{ asset ('new_template/wp-content/plugins/elementor/assets/css/frontend-lite.min.css?ver=3.18.1') }}" media='all'>
+    <link rel='stylesheet' id='swiper-css' href="{{ asset ('new_template/wp-content/plugins/elementor/assets/lib/swiper/css/swiper.min.css?ver=5.3.6') }}" media='all'>
+    <link rel='stylesheet' id='elementor-post-2295-css' href="{{ asset ('new_template/wp-content/uploads/elementor/css/post-2295.css?ver=1701925551') }}" media='all'>
+    <link rel='stylesheet' id='elementor-global-css' href="{{ asset ('new_template/wp-content/uploads/elementor/css/global.css?ver=1701925552') }}" media='all'>
+    <link rel='stylesheet' id='elementor-post-97-css' href="{{ asset ('new_template/wp-content/uploads/elementor/css/post-97.css?ver=1701925552') }}" media='all'>
+    <link rel='stylesheet' id='control-block-patterns-css' href="{{ asset ('new_template/wp-content/plugins/control-block-patterns/assets/css/control-block-patterns.css?ver=1.3.5.4') }}" media='all'>
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin=""><script src="{{ asset('new_template/wp-includes/js/jquery/jquery.min.js?ver=3.7.1" id="jquery-core-js') }}"></script>
+    <script src="{{ asset('new_template/wp-includes/js/jquery/jquery-migrate.min.js?ver=3.4.1" id="jquery-migrate-js') }}"></script>
+    <script src="{{ asset('new_template/wp-content/plugins/woocommerce/assets/js/jquery-blockui/jquery.blockUI.min.js?ver=2.7.0-wc.8.3.1') }}" id="jquery-blockui-js" defer="" data-wp-strategy="defer"></script>
+    <script src="{{ asset('new_template/wp-content/plugins/woocommerce/assets/js/frontend/add-to-cart.min.js?ver=8.3.1') }}" id="wc-add-to-cart-js" defer="" data-wp-strategy="defer"></script>
+    <script src="{{ asset('new_template/wp-content/plugins/woocommerce/assets/js/js-cookie/js.cookie.min.js?ver=2.1.4-wc.8.3.1') }}" id="js-cookie-js" defer="" data-wp-strategy="defer"></script>
+    <script src="{{ asset('new_template/wp-content/plugins/woocommerce/assets/js/frontend/woocommerce.min.js?ver=8.3.1') }}" id="woocommerce-js" defer="" data-wp-strategy="defer"></script>
+    <link rel="https://api.w.org/" href="{{ asset ('new_template/wp-json/index.htm') }}">
+    <link rel="EditURI" type="application/rsd+xml" title="RSD" href="https://jthemes.com/themes/wp/jobbox/xmlrpc.php?rsd">
+    <link rel="alternate" type="application/json+oembed" href="{{ asset ('new_template/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fjthemes.com%2Fthemes%2Fwp%2Fjobbox%2F') }}">
+    <link rel="alternate" type="text/xml+oembed" href="{{ asset ('new_template/wp-json/oembed/1.0/embed-1?url=https%3A%2F%2Fjthemes.com%2Fthemes%2Fwp%2Fjobbox%2F&#038;format=xml') }}">
+    <noscript><style>.woocommerce-product-gallery{ opacity: 1 !important; }</style></noscript>
